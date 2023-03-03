286f6cad6cc4a08b3b787541c1b1511d3c72360b
ysenner <yannicksenner@gmail.com>
2023-03-03 15:25:38 +0100

    fence basic

---
+++ Assets/Scripts/Fence.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fence : MonoBehaviour
+{
+
+    public Vector2 a;
+    public Vector2 b;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Position();
+    }
+
+    void Position()
+    {
+        transform.position = new Vector2((a.x + b.x) / 2, (a.y + b.y) / 2);
+        transform.localScale = new Vector3(transform.localScale.x, (a - b).magnitude, transform.localScale.z); //ugly
+        //Debug.Log(AngleTo(a, b));
+
+        transform.rotation = Quaternion.Euler(0, 0, AngleTo(a, b));
+    }
+
+
+    private float AngleTo(Vector2 a, Vector2 b)
+    {
+        float adjacent = (b.x - a.x);
+        float opposite = (b.y - a.y);
+        float deg =  Mathf.Atan(opposite / adjacent) * Mathf.Rad2Deg;
+        return deg + 90;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
+    private Vector2 input;
+
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        if (input.magnitude > 1){
+            input.Normalize();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(input * speed);
     }
 }
