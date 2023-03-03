4e4187980204b42f238df3af40ab952b864027e6
Julian Walther <julflips@mail.upb.de>
2023-03-03 16:29:38 +0100

    CatDoZoomies

--- Assets/Scripts/CatBehaviour.cs
+++ Assets/Scripts/CatBehaviour.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class CatBehaviour : MonoBehaviour
 {
     public GameObject player;
-    public List<Vector2> posts;
     public float moveCooldown;
     public float strength;
     public float playerDetectionRange;
     public float fenceDetectionRange;
     public float playerWeight;
+    public float foodDetectionRange;
+    public float zoomieProp;
+    public int zoomieLength;
 
     private float lastTimeMoved = 0;
     private Rigidbody2D rigi;
-    
+    private List<Vector2> posts = new List<Vector2>();
+    private List<Vector2> foods = new List<Vector2>();
+    private bool getFood = false;
+    private Vector2 food;
+    private float zoomieCheck = 0.6f;
+    private float lastTimeZoomed = 0;
+    private bool zoomin = false;
+    private int zoomed = 0;
+
     void Start()
     {
         rigi = GetComponent<Rigidbody2D>();
@@ -22,24 +32,84 @@ public class CatBehaviour : MonoBehaviour
     
     void Update()
     {
-        if (Vector2.Distance(player.transform.position, transform.position) <= playerDetectionRange)
+        if (Time.timeSinceLevelLoad - lastTimeZoomed >= zoomieCheck)
         {
-            if (Time.timeSinceLevelLoad - lastTimeMoved >= moveCooldown)
+            Debug.Log("Check for Zoom" + zoomin);
+            if (zoomin)
             {
-                //Debug.Log("Move");
-                Vector2 direction = (player.transform.position - transform.position) * playerWeight;
-                foreach (Vector2 vec in posts)
+                Debug.Log("ZOOOMING");
+                Vector2 direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+                rigi.AddForce(direction * strength * 2);
+                zoomed++;
+                if (zoomed >= zoomieLength)
                 {
-                    if (Vector2.Distance(vec, transform.position) <= fenceDetectionRange)
-                    {
-                        direction += vec;
-                    }
+                    zoomin = false;
+                    zoomed = 0;
                 }
-                direction.Normalize();
-                rigi.AddForce(-direction * strength);
-
-                lastTimeMoved = Time.timeSinceLevelLoad;
             }
+            else
+            {
+                if (Random.value <= zoomieProp)
+                {
+                    zoomin = true;
+                }
+            }
+            
+            lastTimeZoomed = Time.timeSinceLevelLoad;
         }
+        if (Time.timeSinceLevelLoad - lastTimeMoved >= moveCooldown && !zoomin)
+        {
+            getFood = false;
+            foreach (Vector2 vec in foods)
+            {
+                if (Vector2.Distance(vec, transform.position) <= foodDetectionRange)
+                {
+                    getFood = true;
+                    food = vec;
+                }
+            }
+
+            if (getFood)
+            {
+                Vector2 direction = food - (Vector2)transform.position;
+                direction.Normalize();
+                rigi.AddForce(direction * strength);
+            }
+            else
+            {
+                if (Vector2.Distance(player.transform.position, transform.position) <= playerDetectionRange)
+                {
+                    //Debug.Log("Move");
+                    Vector2 direction = (player.transform.position - transform.position) * playerWeight;
+                    foreach (Vector2 vec in posts)
+                    {
+                        if (Vector2.Distance(vec, transform.position) <= fenceDetectionRange)
+                        {
+                            direction += vec;
+                        }
+                    }
+
+                    direction.Normalize();
+                    rigi.AddForce(-direction * strength);
+                }
+            }
+            
+            lastTimeMoved = Time.timeSinceLevelLoad;
+        }
+    }
+
+    public void addPost(Vector2 pos)
+    {
+        posts.Add(pos);
+    }
+
+    public void addFood(Vector2 pos)
+    {
+        foods.Add(pos);
+    }
+
+    public void delFood(Vector2 pos)
+    {
+        foods.Remove(pos);
     }
 }
---
+++ Assets/Scripts/CatManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CatManager : MonoBehaviour
+{
+    public int areaX = 10;
+    public int areaY = 5;
+    public int numberOfCats;
+    public GameObject cat;
+    public GameObject player;
+    public List<GameObject> cats;
+
+    void Start()
+    {
+        //Spawn cats around 0 in a areaX times areaY zone
+        for (int i = 0; i < numberOfCats; i++)
+        {
+            GameObject tempCat = Instantiate(cat, getRandomPos(areaX, areaY), Quaternion.identity);
+            tempCat.GetComponent<CatBehaviour>().player = player;
+            cats.Add(tempCat);
+        }
+    }
+
+    private Vector2 getRandomPos(int x, int y)
+    {
+        return new Vector2(Random.Range(-x / 2, x / 2), Random.Range(-y/2, y/2));
+    }
+
+    private void addFood(Vector2 pos)
+    {
+        foreach (GameObject tempCat in cats)
+        {
+            tempCat.GetComponent<CatBehaviour>().addFood(pos);
+        }
+    }
+    
+    private void delFood(Vector2 pos)
+    {
+        foreach (GameObject tempCat in cats)
+        {
+            tempCat.GetComponent<CatBehaviour>().delFood(pos);
+        }
+    }
+    
+    private void addPost(Vector2 pos)
+    {
+        foreach (GameObject tempCat in cats)
+        {
+            tempCat.GetComponent<CatBehaviour>().addPost(pos);
+        }
+    }
+}
